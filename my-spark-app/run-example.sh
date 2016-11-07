ad2bc3a363dd92c1f2d6dbb55efc1faa33cfec52
Joao Antonio Ferreira <joao.parana@gmail.com>
2016-11-07 08:07:08 -0200

    Algumas alterações

--- my-spark-app/run-example.sh
+++ my-spark-app/run-example.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-/opt/jdk1.8.0_91/bin/java \
-    -cp /usr/local/spark/conf/:/usr/local/spark/jars/* \
-    -Xmx1g \
-    org.apache.spark.deploy.SparkSubmit \
-    --jars /usr/local/spark/examples/jars/scopt_2.11-3.3.0.jar,/usr/local/spark/examples/jars/spark-examples_2.11-2.0.0.jar \
-    --class org.apache.spark.examples.$1 spark-internal
+cd /desenv/java/myspark
+echo "Current Directory: `pwd`"
+echo "java -Xmx1g -cp target/myspark-1.0-SNAPSHOT.jar spark.Tutorial"
+java -Xmx1g -cp target/myspark-1.0-SNAPSHOT.jar spark.Tutorial
+
+# /opt/jdk1.8.0_91/bin/java \
+#     -cp /usr/local/spark/conf/:/usr/local/spark/jars/* \
+#     -Xmx1g \
+#     org.apache.spark.deploy.SparkSubmit \
+#     --jars /usr/local/spark/examples/jars/scopt_2.11-3.3.0.jar,/usr/local/spark/examples/jars/spark-examples_2.11-2.0.0.jar \
+#     --class org.apache.spark.examples.$1 spark-internal
